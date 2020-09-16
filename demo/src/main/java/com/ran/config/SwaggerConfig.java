df54afc5315f81c600084181fb632b52965d4d55
ran7777 <17858274053@163.com>
2020-09-16 17:06:28 +0800

    first commmit

---
+++ demo/src/main/java/com/ran/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.ran.config;
+
+
+import com.ran.common.config.BaseSwaggerConfig;
+import com.ran.common.domain.SwaggerProperties;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger相关配置
+ * Created by macro on 2019/4/8.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends BaseSwaggerConfig {
+
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.ran.controller")
+                .title("mall-demo系统")
+                .description("SpringBoot版本中的一些示例")
+                .contactName("macro")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+
+}
---
+++ demo/src/main/java/com/ran/service/DemoService.java
@@ -0,0 +1,25 @@
+package com.ran.service;
+
+
+import com.ran.dto.PmsBrandDto;
+import com.ran.model.PmsBrand;
+
+import java.util.List;
+
+
+/**
+ * DemoService接口
+ */
+public interface DemoService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrandDto pmsBrandDto);
+
+    int updateBrand(Long id, PmsBrandDto pmsBrandDto);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
